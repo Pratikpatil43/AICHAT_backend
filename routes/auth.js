e85703995e341c98142bc3eace87a4f3fb9d69e2
Pratikpatil43 <patilpratik4388@gmail.com>
2025-02-14 13:42:26 +0530

    Update auth.js

--- routes/auth.js
+++ routes/auth.js
@@ -15,6 +15,9 @@ router.post("/register", async (req, res) => {
 
     const hashedPassword = await bcrypt.hash(password, 10);
     const user = new User({ name, email, password: hashedPassword, gender, chatPreference });
+     const token = jwt.sign({ userId: user._id }, process.env.JWT_SECRET, { expiresIn: "1d" });
+
+    res.json({ token, user });
     
     await user.save();
     res.status(201).json({ msg: "User registered successfully" });
@@ -25,23 +28,23 @@ router.post("/register", async (req, res) => {
 });
 
 // Login User
-router.post("/login", async (req, res) => {
-  try {
-    const { email, password } = req.body;
+// router.post("/login", async (req, res) => {
+//   try {
+//     const { email, password } = req.body;
     
-    const user = await User.findOne({ email });
-    if (!user) return res.status(400).json({ msg: "User not found" });
+//     const user = await User.findOne({ email });
+//     if (!user) return res.status(400).json({ msg: "User not found" });
 
-    const isMatch = await bcrypt.compare(password, user.password);
-    if (!isMatch) return res.status(400).json({ msg: "Invalid credentials" });
+//     const isMatch = await bcrypt.compare(password, user.password);
+//     if (!isMatch) return res.status(400).json({ msg: "Invalid credentials" });
 
-    const token = jwt.sign({ userId: user._id }, process.env.JWT_SECRET, { expiresIn: "1d" });
+//     const token = jwt.sign({ userId: user._id }, process.env.JWT_SECRET, { expiresIn: "1d" });
 
-    res.json({ token, user });
+//     res.json({ token, user });
 
-  } catch (error) {
-    res.status(500).json({ msg: "Server error" });
-  }
-});
+//   } catch (error) {
+//     res.status(500).json({ msg: "Server error" });
+//   }
+// });
 
 module.exports = router;
