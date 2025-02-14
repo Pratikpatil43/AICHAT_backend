1bd71f70ec8739e16de2063b92cb44d561351ac7
Pratikpatil43 <patilpratik4388@gmail.com>
2025-02-14 14:19:02 +0530

    Update User.js

--- models/User.js
+++ models/User.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 const UserSchema = new mongoose.Schema({
   name: { type: String, required: true },
-  email: { type: String, required: true, unique: true },
+  username: { type: String, required: true, unique: true },
   password: { type: String, required: true },
   gender: { type: String, enum: ["male", "female"], required: true },
   chatPreference: { type: String, enum: ["funny", "flirty", "romantic"], default: "funny" }
